c251e59e6ffd2ffcbffd1abfc1b2525c0fa866f7
filipzdelar <filipzdelar@gmail.com>
2023-03-24 18:58:34 +0100

    App location7

---
+++ src/app/home-to-talk/home-to-talk.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-to-talk',
+  templateUrl: './home-to-talk.component.html',
+  styleUrls: ['./home-to-talk.component.css']
+})
+export class HomeToTalkComponent {
+
+}
